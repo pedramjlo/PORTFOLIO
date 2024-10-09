0b2ebfb8be0a58b7dfb4e4d3bad8e12f07917ca5
pedramjlo <pedram_sif@tuta.io>
2024-10-09 17:06:31 +0330

    added certs

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,9 @@ import Django from '../images/django.webp'
 
 
 import '../styles/projects.css';
-import GoToButton from './GoToBtn';
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 
 const Projects = () => {
@@ -31,33 +33,49 @@ const Projects = () => {
         currentLanguage(i18n.language);
     }, [i18n.language]);
 
+    const renderProject = (imgSrc, title, ingreds, date, explain) => (
+        <div className='section-content'>
+            <img src={imgSrc} alt={title} />
+            <span className='project-title'>{title}</span>
+            <p className='project-ingreds'>{ingreds}</p>
+            {date && <span className='project-date'>{date}</span>}
+            <span className='project-exp'>{explain}</span>
+        </div>
+    );
+
+
     return(
         <div>
             {isMobile ? (
                 <div className='section-container'>
+
                     <div className='section-title-container'>
                         <span className='section-title'>{t('projects')}</span>
                     </div>
+
                     <div className='section-content'>
                         <img src={DaeiAli} alt="DaeiAli" />
                         <span className='project-title'>{t('daeiAli')}</span>
                         <p className='project-ingreds'>{t('daeiAliingreds')}</p>
                         <span className='project-date'>{t('daeiAliDelivery')}</span>
                         <span className='project-exp'>{t('daeiAliExplain')}</span>
-                        <GoToButton />
+
+                        <p><a class="link-opacity-100" href="https://daei-fastfood-ali.ir">{t('gotoButton')}</a></p>
+                        <p><a class="link-opacity-100" href="https://github.com/pedramjlo/Daei-Ali-ORG.git">{t('sourceCode')}</a></p>
                     </div>
 
-                    <br />
                     <hr />
                     <div className='section-content'>
                         <img src={Django} alt="Django" />
                         <span className='project-title'>{t('blogApp')}</span>
                         <p className='project-ingreds'>{t('blogAppingreds')}</p>
                         <span className='project-exp'>{t('blogAppExplain')}</span>
-                        <GoToButton />
+
+                        <p><a class="link-opacity-100" href="https://github.com/pedramjlo/alketas_blog.git">{t('sourceCode')}</a></p>
+
+
                     </div>
                     
-                    <br />
                     <hr />
                     <div className='section-content'>
                         <img src={Django} alt="Django" />
@@ -65,15 +83,65 @@ const Projects = () => {
                         <p className='project-ingreds'>{t('todoAppingreds')}</p>
                         <span className='project-exp'>{t('todoAppExplain')}</span>
                     </div>
-                    <GoToButton />
+
+                    <button type='button' url="https://www.example.com">Go to Example</button>
                     <br />
 
                 </div>
 
             ) : (
-                <div>
+                <div className='section-container'>
+
+                <div className='section-title-container'>
+                    <span className='section-title'>{t('projects')}</span>
+                </div>
+
+                <div className='section-content'>
+                    <img src={DaeiAli} alt="DaeiAli" />
+                    <span className='project-title'>{t('daeiAli')}</span>
+                    <p className='project-ingreds'>{t('daeiAliingreds')}</p>
+                    <span className='project-date'>{t('daeiAliDelivery')}</span>
+                    <span className='project-exp'>{t('daeiAliExplain')}</span>
+
+                    <div className='section-content-btns'>
+                        <p><a class="link-opacity-100 btn" href="https://daei-fastfood-ali.ir" style={{ color: "#ffffff" }}>{t('gotoButton')}</a></p>
+                        <p><a class="link-opacity-100" href="https://github.com/pedramjlo/Daei-Ali-ORG.git">{t('sourceCode')}</a></p>
+                    </div>
+                </div>
+
+                <hr />
+                <div className='section-content'>
+                    <img src={Django} alt="Django" />
+                    <span className='project-title'>{t('blogApp')}</span>
+                    <p className='project-ingreds'>{t('blogAppingreds')}</p>
+                    <span className='project-exp'>{t('blogAppExplain')}</span>
+
+                    <div className='section-content-btns'>
+                        <p><a class="link-opacity-100 btn" href="https://github.com/pedramjlo/alketas_blog.git" style={{ color: "#ffffff" }}>{t('sourceCode')}</a></p>
+                    </div>
+
 
                 </div>
+                
+                <hr />
+                <div className='section-content'>
+                    <img src={Django} alt="Django" />
+                    <span className='project-title'>{t('todoApp')}</span>
+                    <p className='project-ingreds'>{t('todoAppingreds')}</p>
+                    <span className='project-exp'>{t('todoAppExplain')}</span>
+
+                    <div className='section-content-btns'>
+                        <p><a class="link-opacity-100 btn" href="https://github.com/pedramjlo/alketas_blog.git" style={{ color: "#ffffff" }}>{t('sourceCode')}</a></p>
+                    </div>
+
+                </div>
+
+                
+
+
+            </div>
+
+
             )}
         </div>
     );
